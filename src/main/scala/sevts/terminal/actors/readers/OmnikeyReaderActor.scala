19db28087d50a2cd4b268d3c4d78a106044de330
SergeyBulanenkov <sbulanenkov@gmail.com>
2025-07-30 19:26:06 +0300

    add omnikey rfid write user data

--- src/main/scala/sevts/terminal/actors/readers/OmnikeyReaderActor.scala
+++ src/main/scala/sevts/terminal/actors/readers/OmnikeyReaderActor.scala
@@ -5,8 +5,14 @@ import java.util.concurrent.TimeUnit
 import javax.smartcardio._
 import akka.actor.{Actor, ActorRef, Props}
 import com.typesafe.scalalogging.LazyLogging
+import sevts.remote.protocol.Protocol.ServerMessage
+import sevts.server.protocol.TerminalEvent.WriteRfidUserMemoryEvent
+import sevts.terminal.actors.readers.OmnikeyWriteReaderActor.{IdleData, WaitCard}
+import sevts.terminal.actors.readers.Rfid9809ReaderActor.ComPort
 import sevts.terminal.config.Settings.DeviceConfig
 
+import java.nio.{ByteBuffer, IntBuffer}
+import scala.annotation.tailrec
 import scala.concurrent.blocking
 import scala.concurrent.duration._
 import scala.language.postfixOps
@@ -40,9 +46,7 @@ object OmnikeyReaderActor {
 
 class OmnikeyReaderActor(listener: ActorRef, device: DeviceConfig)
   extends Actor
-    with LazyLogging
-    //with SmartCardOperations
-    {
+    with LazyLogging {
 
   import OmnikeyReaderActor._
 
@@ -51,16 +55,51 @@ class OmnikeyReaderActor(listener: ActorRef, device: DeviceConfig)
 
   val portName = device.parameters.getString("portName")
   val delay = Duration(device.parameters.getInt("delay"), TimeUnit.MILLISECONDS)
+  val writeAttemptDelay = Duration(device.parameters.getInt("writeAttemptDelay"), TimeUnit.MILLISECONDS)
 
 
   override def preStart = {
     logger.info("Starting Omnikey reader...")
+    context.system.eventStream.subscribe(self, classOf[ServerMessage])
     connect()
   }
 
   override def postStop = {
+    context.system.eventStream.unsubscribe(self, classOf[ServerMessage])
     logger.error("Actor dead")
   }
+
+  def ready(terminal: CardTerminal): Receive = {
+    case msg: ServerMessage =>
+      logger.info(s"ServerMessage: ${msg.msg}")
+      msg.msg match {
+        case data: WriteRfidUserMemoryEvent =>
+          logger.info(s"Omnikey: Write user memory event ${data.value}")
+          writeCard(terminal, data.value, 10) match {
+            case Some(true) =>
+              //sender() ! WriteOk
+              context.system.eventStream.unsubscribe(self, classOf[ServerMessage])
+              context.become(receive)
+            case _ =>
+              //sender() ! WriteError
+              context.system.eventStream.unsubscribe(self, classOf[ServerMessage])
+              context.become(receive)
+          }
+      }
+
+    case Command.ReadCard(terminal) =>
+      tryReadCard(terminal).foreach { result =>
+        logger.info(s"Read value: $result")
+        listener ! ReadersActor.DeviceEvent.DataReceived(device.name, result.stripSuffix("9000"))
+        context.system.scheduler.scheduleOnce(delay, self, Command.WriteCard(terminal))
+      }
+
+    case Command.ReconnectCard =>
+      context.system.eventStream.unsubscribe(self, classOf[ServerMessage])
+      context.become(receive)
+      self ! Command.ReconnectCard
+  }
+
 
   def receive = {
 
@@ -73,27 +112,14 @@ class OmnikeyReaderActor(listener: ActorRef, device: DeviceConfig)
       }
 
     case Command.StartTerminalRead(terminal: CardTerminal) =>
-      context.system.scheduler.scheduleOnce(delay, self, Command.ReadCard(terminal))
-
-    case Command.ReadCard(terminal) =>
-      tryReadCard(terminal).foreach { result =>
-        logger.info(s"Read value: $result")
-        listener ! ReadersActor.DeviceEvent.DataReceived(device.name, result.stripSuffix("9000"))
-        context.system.scheduler.scheduleOnce(delay, self, Command.WriteCard(terminal))
-      }
-
-    case Command.WriteCard(terminal) =>
-      val url = "https://google.com"
-      OmnikeyWriter.writeUrlNdefAuto(terminal, url).foreach { success =>
-        if (success) {
-          logger.info(s"Successfully wrote URL: $url")
-        }
-      }
+      context.system.eventStream.subscribe(self, classOf[ServerMessage])
+      context.become(ready(terminal))
       context.system.scheduler.scheduleOnce(delay, self, Command.ReadCard(terminal))
 
     case msg =>
       logger.error("Unknown message received ${msg.toString}")
   }
+
 
   private def connect() = {
     import scala.jdk.CollectionConverters._
@@ -149,6 +175,22 @@ class OmnikeyReaderActor(listener: ActorRef, device: DeviceConfig)
         logger.error(e.getMessage, e)
         self ! Command.ReconnectCard
         None
+    }
+  }
+
+  @tailrec
+  private def writeCard(terminal: CardTerminal, data: String, counter: Int): Option[Boolean] = {
+    if (counter > 0) {
+      val result = OmnikeyWriter.writeUrlNdefAuto(terminal, data)
+      result match {
+        case Some(true) => result
+        case _ =>
+          logger.error(s"Write nfc error for data: $data, retrying...")
+          Thread.sleep(writeAttemptDelay.toMillis)
+          writeCard(terminal, data, counter - 1)
+      }
+    } else {
+      None
     }
   }
 
